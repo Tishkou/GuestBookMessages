0ecc38f60ab9f245c303aa72f7f7bb81f2487bdc
Opiclues <Opiclues@TISHKOPC>
2022-08-11 22:16:15 +0300

    Add project files.

---
+++ GuestBookProps/Program.cs
@@ -0,0 +1,45 @@
+﻿using PartGuesterLibrary.Models;
+
+
+
+List<GuestLibararyClass> guests = new();
+
+GetInformationFromUser(guests);
+
+PrintInformations(guests);
+
+
+static void PrintInformations(List<GuestLibararyClass> guests)
+{
+    foreach (GuestLibararyClass guest in guests)
+    {
+        Console.WriteLine(guest.FullStuff);
+    }
+}
+static void GetInformationFromUser(List<GuestLibararyClass> guests)
+{
+    string StillCounting = "";
+
+    do
+    {
+        GuestLibararyClass guest = new GuestLibararyClass();
+        guest.FirstName = Messages("What's your first name: ");
+        guest.LastName = Messages("What's your last name:  ");
+        guest.MessageToHost = Messages("What's your message to the host: ");
+        StillCounting = Messages("Contiue Yes/No: ");
+        guests.Add(guest);
+        Console.Clear();
+    } while (StillCounting.ToLower() == "yes");
+
+}
+static string Messages(string message)
+{
+    Console.WriteLine(message);
+    string output = Console.ReadLine();
+    return output;
+}
+
+
+
+
+
---
+++ PartGuesterLibrary/Models/GuestLibararyClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartGuesterLibrary.Models
+{
+    public class GuestLibararyClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string MessageToHost { get; set; }
+
+        private string _FullStuff;
+
+        public string FullStuff
+        {
+            get 
+            {
+                return $"{FirstName} {LastName}: {MessageToHost}";
+            }
+            
+        }
+
+
+    }
+}
